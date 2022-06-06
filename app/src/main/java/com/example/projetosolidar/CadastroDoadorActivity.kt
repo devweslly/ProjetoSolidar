843f9c6aabc4c3b57eec645335d0f445304223ec
Weslly Viana <devweslly@gmail.com>
2022-06-06 12:09:14 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/projetosolidar/CadastroDoadorActivity.kt
@@ -0,0 +1,27 @@
+package com.example.projetosolidar
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class CadastroDoadorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_doador)
+
+        // findViewById(id) - acessando o botão
+        val botaoTelaCadastDoador = findViewById<Button>(R.id.botao_criar_doador)
+
+        // Ação de click
+        botaoTelaCadastDoador.setOnClickListener {
+            // chamada do metodo
+            irTelaCadastroPerfil()
+        }
+    }
+
+    private fun irTelaCadastroPerfil(){
+        val telaCadasPerfilDoador = Intent(this, CadastroPerfilDoadorActivity::class.java)
+        startActivity(telaCadasPerfilDoador)
+    }
+}
---
+++ app/src/main/java/com/example/projetosolidar/CadastroPerfilDoadorActivity.kt
@@ -0,0 +1,24 @@
+package com.example.projetosolidar
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class CadastroPerfilDoadorActivity : AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_perfil_doador)
+
+        val botaoCriarContaDoador = findViewById<Button>(R.id.botao_criar_doador)
+
+        botaoCriarContaDoador.setOnClickListener {
+            irTelaCriarProduto()
+        }
+    }
+
+    private fun irTelaCriarProduto(){
+        val telaCadasProduto = Intent(this, CadastroProdutoDoador::class.java)
+        startActivity(telaCadasProduto)
+    }
+}
