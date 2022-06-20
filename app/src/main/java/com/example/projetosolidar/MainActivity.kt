7fb6399ee2f1708b6f3a508ce4adf09be26b71de
Weslly Viana <devweslly@gmail.com>
2022-06-20 16:21:02 -0300

    Projeto Solidar - Menu Navogation

--- app/src/main/java/com/example/projetosolidar/MainActivity.kt
+++ app/src/main/java/com/example/projetosolidar/MainActivity.kt
@@ -2,13 +2,29 @@ package com.example.projetosolidar
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.projetosolidar.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+    private val navController by lazy {
+        supportFragmentManager.findFragmentById(R.id.fragmentContainerView)!!.findNavController()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupBottomNavigation()
+    }
+
+    private fun setupBottomNavigation() {
+        with(binding.bottomNavigation) { setupWithNavController(navController) }
     }
 }
